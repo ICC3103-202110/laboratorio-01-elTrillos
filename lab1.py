66d2a18f7e9714320e972614b4a39614e3df342a
elTrillos <strillos@miuandes.cl>
2021-03-24 20:32:30 -0300

    Despues de demasiado rato, logre descubrir que las listas dentro de listas pueden ser dependientes, asi que todo desde ahora deberia salir mas facil

--- lab1.py
+++ lab1.py
@@ -22,8 +22,10 @@ def cardgen(size):
     listaOG = []
     listatemp = []
     listatemp2 = []
+    listatemp3 = []
     daBoard = []
     daBoard2 =[]
+    daBoard3 =[]
     k1 = 1
     n1 = 0
     n2 = 0
@@ -36,7 +38,7 @@ def cardgen(size):
         listaOG.append(k1)
         listaOG.append(k1)
         k1 +=1
-    print(listaOG)
+    #print(listaOG)
     random.shuffle(listaOG)
     print(listaOG)
     while n1 < alto:
@@ -44,22 +46,26 @@ def cardgen(size):
             if listaOG[0] < 10:
                 listatemp.append("0"+str(listaOG[0]))
                 listatemp2.append("▓▓")
+                listatemp3.append("▓▓")
             else:
                 listatemp.append(str(listaOG[0]))
                 listatemp2.append("▓▓")
+                listatemp3.append("▓▓")
             listaOG.pop(0)
             n2 += 1
         daBoard.append(listatemp)
         daBoard2.append(listatemp2)
+        daBoard3.append(listatemp3)
         listatemp = []
         listatemp2 = []
+        listatemp3 = []
         n2 = 0
         n1 += 1
-    print(daBoard)
+    #print(daBoard)
     print(daBoard2)
-    return daBoard,daBoard2,ancho,alto
+    return daBoard,daBoard2,ancho,alto,daBoard3
 
-def gameui(boardrial,boardimg):
+def gameui(boardimg):
     for i in boardimg:
         for j in i:
             print(f"{j:>3}",end=(' '))
@@ -67,49 +73,83 @@ def gameui(boardrial,boardimg):
 
 def gameplay():
     curpart = 1
-    numss = []
+    numss = ["",""]
     score1 = 0
     score2 = 0
     curplaya = 1
     tempBoard = []
     playing = True
     ans = []
+    ans1 = []
+    ans2 = []
     print("===============================")
     print("  Bienvenido a memorice v1.0   ")
     print("===============================")
     sizee = startui()
     print(sizee)
-    daboi,daboi2,ancho,alto = cardgen(sizee)
-    gameui(daboi,daboi2)
+    daboi,daboi2,ancho,alto,tempBoard = cardgen(sizee)
+    gameui(daboi2)
     qnparte = random.randint(1,2)
     curplaya = qnparte
     print("comienza el jugador", curplaya)
     while playing == True:
         asking = True
+        print("a")
         while asking == True:
             print("Jugador", curplaya , "escoja la coordenada de la carta (x y) numero", curpart)
             ans = input().split(" ")
             if ans[0].isnumeric() and ans[1].isnumeric() == True:
                 ans[0] = int(ans[0])
                 ans[1] = int(ans[1])
+                if curpart == 1:
+                    ans1 = list(ans)
+                else:
+                    ans2 = list(ans)
                 if ans[0] <= ancho and ans[1] <= alto:
                     print("ok")
-                    
-                    tempBoard = list(daboi2)
+                    print(tempBoard)
+                    print(daboi2)
+                    print(daboi)
+                    print("")
+
+
+                    print(tempBoard)
+                    print(daboi2)
+                    print(daboi)
+                    print("")
                     tempBoard[ans[1]-1][ans[0]-1] = daboi[ans[1]-1][ans[0]-1]
-                    gameui(daboi,tempBoard)
+                    print(tempBoard)
+                    print(daboi2)
+                    print(daboi)
+                    gameui(tempBoard)
                     numss[curpart-1] = daboi[ans[1]-1][ans[0]-1]
                     if curpart == 1:
                         curpart = 2
+                        print(numss)
+                        
+                        asking = True
                     else:
-                        asking = False
-                    return ans
+                        if ans1 == ans2:
+                            print("escribe 2 coordenadas DISTINTAS")
+                            curpart = 1
+                            numss = ["",""]
+                            tempBoard = daboi2
+                            print(daboi2)
+                            asking = True
+                        else:
+                            asking = False
+                    
                 else: 
                     print("error, escribir valores dentro del tamaño de la tabla de juego")
             else:
                 print("error, seguir el formato por favor")
-        if curpart[0] == curpart[1]:
+        if numss[0] == numss[1]:
             print("oke")
+            if curplaya == 1:
+                score1 +=1
+            else:
+                score2 +=1
+            
         else:
             print("dou")
         playing = False
