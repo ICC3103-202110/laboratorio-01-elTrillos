0a4bbc3517642bcae6d6c0ec079d4d602db84d7b
elTrillos <strillos@miuandes.cl>
2021-03-26 18:28:50 -0300

    commit final

--- lab1.py
+++ lab1.py
@@ -30,18 +30,17 @@ def cardgen(size):
     n1 = 0
     n2 = 0
     aa = (reduce(list.__add__, ([i, numero//i] for i in range(1, int(numero**0.5) + 1) if numero % i == 0))) 
-    #lo obtuve de stackoverflow
+    #lo obtuve de stackoverflow, entrega los factores que al multiplicarse llegan al numero buscado
     alto = aa[-1]
-    
     ancho = aa[-2]
-    print(alto,ancho)
+    #los ultimos dos valores de la lista que genera siempre 
+    # van a ser los que entreguen la matriz mas cuadrada posible
+    #print(alto,ancho)
     while k1 <= size:
         listaOG.append(k1)
         listaOG.append(k1)
         k1 +=1
-    #print(listaOG)
     random.shuffle(listaOG)
-    #print(listaOG)
     while n1 < alto:
         while n2 < ancho:
             if listaOG[0] < 10:
@@ -62,18 +61,13 @@ def cardgen(size):
         listatemp3 = []
         n2 = 0
         n1 += 1
-    #print(daBoard)
-    #print(daBoard2)
     return daBoard,daBoard2,ancho,alto,daBoard3
-
 
 def gameui(boardimg):
     for i in boardimg:
         for j in i:
             print(f"{j:>3}",end=(' '))
         print()   
-
-
 
 def gameplay():
     curpart = 1
@@ -92,7 +86,6 @@ def gameplay():
     print("   Copyright TrillosSoft 2021   ")
     print("================================")
     sizee = startui()
-    #print(sizee)
     daboi,daboi2,ancho,alto,tempBoard = cardgen(sizee)
     qnparte = 1
     curplaya = qnparte
@@ -100,11 +93,8 @@ def gameplay():
     gameui(tempBoard)
     while playing == True:
         asking = True
-        #print("a")
-        #gameui(tempBoard)
         while asking == True:
-            #gameui(tempBoard)
-            print("Player 1", curplaya , "choose the card (x y) number", curpart)
+            print("Player ", curplaya , "choose the card (x y) number", curpart)
             ans = input().split(" ")
             if ans[0].isnumeric() and ans[1].isnumeric() == True:
                 ans[0] = int(ans[0])
@@ -122,51 +112,40 @@ def gameplay():
                         numss[curpart-1] = daboi[ans[1]-1][ans[0]-1]
                         if curpart == 1:
                             curpart = 2
-                            #print(numss)
-                            
                             asking = True
                         else:
                             if ans1 == ans2:
                                 print("please write 2 DIFFERENT coordinates")
                                 curpart = 1
                                 numss = ["",""]
-                                #tempBoard = list(daboi2)
                                 for x in daboi2:
                                     tempBoard.append(list(x))
-                                #print(daboi2)
                                 asking = True
                             else:
                                 curpart = 1
-                                gameui(tempBoard)
+                                #gameui(tempBoard)
                                 asking = False
                     else:
-                        print("error, write coordinates not previously discovered")
-                    
+                        print("error, write coordinates not previously discovered")        
                 else: 
                     print("error, please insert values inside the limits")
             else:
                 print("error, follow the format please")
         if numss[0] == numss[1]:
-            #gameui(tempBoard)
-            print("Player" ,curplaya, "found a pair!")
-            
+            print("Player" ,curplaya, "found a pair!")         
             if curplaya == 1:
-                curplaya = 2
+                curplaya = 1
                 score1 +=1
             else:
-                curplaya = 1
+                curplaya = 2
                 score2 +=1
-            #print(ans1)
-            #print(ans2)
             tempBoard[ans1[1]-1][ans1[0]-1] = "  "
             tempBoard[ans2[1]-1][ans2[0]-1] = "  "
             daboi2[ans1[1]-1][ans1[0]-1] = "  "
             daboi2[ans2[1]-1][ans2[0]-1] = "  "
             
         else:
-            #gameui(tempBoard)
             print("Player" ,curplaya ,"didn´t find a pair!")
-
             if curplaya == 1:
                 curplaya = 2
             else:
@@ -174,9 +153,8 @@ def gameplay():
             tempBoard = []
             for x in daboi2:
                 tempBoard.append(list(x))
-            #tempBoard = list(daboi2)
-            #print("dou")
-        print("Player 2:",score1 ,"/ Player 2:", score2)
+        print(" ")
+        print("Player 1:",score1 ,"/ Player 2:", score2)
         if score1 + score2 == sizee:
             if score1 > score2:
                 print("Player 1 won!" )
@@ -189,6 +167,5 @@ def gameplay():
                 break
             playing = False
         playing = True
-
-    
+  
 gameplay()
